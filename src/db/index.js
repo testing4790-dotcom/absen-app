537cf1a92db2a3e4ca535373b08470450ee09259
testing4790-dotcom <testing4790@gmail.com>
2026-01-22 22:02:54 +0600

    Create index.js

---
+++ src/db/index.js
@@ -0,0 +1,10 @@
+import pg from "pg";
+
+const { Pool } = pg;
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL?.includes("render.com")
+    ? { rejectUnauthorized: false }
+    : undefined
+});
